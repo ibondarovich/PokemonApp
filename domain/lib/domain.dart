f4d57e4dd4a4707524e6f6516fb1d3b1d6f955b9
ibondarovich <ilona.bondarovich@gmail.com>
2023-06-22 18:19:53 +0300

    add bloc implementation

--- domain/lib/domain.dart
+++ domain/lib/domain.dart
@@ -1,3 +1,7 @@
 library domain;
 
 export 'package:domain/repositories/pokemons_repository.dart';
+export 'package:domain/models/pokemon_model.dart';
+export 'package:domain/models/pokemon_detailed_model.dart';
+export 'package:domain/usecases/fetch_pokemons_usecase.dart';
+export 'package:domain/usecases/fetch_pokemon_details_usecase.dart';
