84e75f1f8458cb29aab526f4f8b91ef7c24d050c
ibondarovich <ilona.bondarovich@gmail.com>
2023-06-22 18:19:16 +0300

    add bloc implementation

---
+++ features/main_view/lib/src/bloc/main_view/bloc.dart
@@ -0,0 +1,28 @@
+import 'package:bloc/bloc.dart';
+import 'package:domain/domain.dart';
+import 'package:meta/meta.dart';
+
+part 'event.dart';
+part 'state.dart';
+
+class MainViewBloc extends Bloc<MainViewEvent, MainViewState> {
+  final FetchPokemonsUseCase _getPokemonsUseCase;
+  List<PokemonModel> pokemons = <PokemonModel>[];
+
+  MainViewBloc({
+    required FetchPokemonsUseCase getPokemonsUseCase
+  }) : _getPokemonsUseCase = getPokemonsUseCase,
+        super(EmptyState()) {
+    on<InitEvent>(_init);
+  }
+
+  void _init(InitEvent event, Emitter<MainViewState> emit) async {
+    emit(LoadingState());
+    try{
+      pokemons = await _getPokemonsUseCase.execute(20);
+      emit(LoadedState(pokemons: pokemons));
+    }catch(e,_){
+      emit(ErrorState(errorMessage: e.toString()));
+    }
+  }
+}
